92469580ebe192b5d8924c487bdc308252c2e332
Jun Chen <junc76@gmail.com>
2013-03-14 18:21:43 +0800

    Update the euler

--- gmock-1.6.0/projectEuler/Problems/PythonTool/Euler/Euler.py
+++ gmock-1.6.0/projectEuler/Problems/PythonTool/Euler/Euler.py
@@ -1,3 +1,17 @@
+import math
+
+def problem_13():
+    data = []
+    def getData():
+        fp = open("problem13.txt", "r")
+        for line in fp:
+            line = line.rstrip()
+            data.append(int(line))
+    getData()
+    s = str(sum(data))
+    for i in range(10):
+        print (s[i], end='') 
+
 def problem_15():
     def pascal_row(n):
         r = [1]
@@ -86,6 +100,31 @@ def problem_17():
     total += len(dict[1000])
     print (total)
 
+def problem_18():
+    data = []
+    dict = {}
+    def getData():
+        fp = open("problem18.txt", "r")
+        for line in fp:
+            line = line.rstrip()
+            a = [int(x) for x in line.split()]
+            data.append(a)
+
+    getData()
+    m = len(data[-1])
+    for i in range(m):
+        for j in range(i+1):
+            left = 0
+            right = 0
+            if (j>0):
+                left = dict[(i-1,j-1)]
+            if (j<i):
+                right = dict[(i-1,j)]
+            parent = max(left, right)
+            dict[(i,j)] = parent + data[i][j]
+    maxvalue = max(dict.values())
+    print(maxvalue)
+
 def problem_19():
     dict={1:31, 2:28, 3:31, 4:30, 5:31,6:30, \
           7:31, 8:31, 9:30,10:31,11:30,12:31}
@@ -116,30 +155,111 @@ def problem_19():
             days += daysPerYear(i)
         return days
 
-    #print(not isLeapYear(100))
-    #print(isLeapYear(400))
-    #print(isLeapYear(12))
-    #print(not isLeapYear(101))
-    #print(days(1900))
-    #print(days(2000))
-    #print(dict)
-    print (daysFrom1900(1901,1))
-    print (daysFrom1900(1902,1))
-    print (daysFrom1900(1903,1))
-    print (daysFrom1900(1904,1))
-    print (daysFrom1900(1905,1))
     sundays=0
-    for i in range(1901, 2000):
-        for j in range(1, 12):
+    for i in range(1901, 2001): # I used 2000 first!
+        for j in range(1, 13): # I used 12 first!
             days = daysFrom1900(i,j)
             if (days % 7 == 0):
                 sundays += 1
                 print("(%d,%d)"%(i,j))
     print (sundays)
 
+def problem_22():
+    data=[]
+    def getData():
+        buff=[]
+        fp = open("problem22.txt","r")
+        for line in fp:
+            buff = line.split(",")
+            data.extend([(lambda s:s.strip('"'))(x) for x in buff])
+    
+    def sortData():
+        data.sort()
+
+    def worth(word):
+        s = [(lambda x:ord(x)- 64)(ch) for ch in word]
+        return sum(s)
+
+    getData()
+    sortData()
+    loop = len(data)
+    i=1
+    total=0
+    while(i<loop+1):
+        total += i*worth(data[i-1])
+        i += 1
+    print(total)
+
+def problem_23():
+    def isAbundant(num):
+        total = 1
+        i = 2
+        l = math.ceil(num**0.5)
+        divs=list(filter(lambda x:num%x==0, range(2,l)))
+        s = [(lambda x:num//x)(x) for x in divs]
+        divs += s
+        divs.append(1)
+        if (l*l == num):
+            divs.append(l)
+        return sum(divs) > num
+
+    count = 0
+    i = 2
+    #s = set([])
+    #while (i<28124):
+    #    if (isAbundant(i)):
+    #        s.add(i)
+    #    i+=1
+    s = set(filter(lambda x:isAbundant(x), range(2,28124)))
+
+    total = 0
+    i = 1
+    while (i<28124):
+        found = False
+        for x in s:
+            y = i - x
+            if (y <=0):
+                continue
+            if (y in s):
+                found = True
+                break
+        if (not found):
+            total += i
+        i += 1
+    print(total)
+
+def problem_67():
+    data = []
+    dict = {}
+    def getData():
+        fp = open("problem67.txt", "r")
+        for line in fp:
+            line = line.rstrip()
+            a = [int(x) for x in line.split()]
+            data.append(a)
+
+    getData()
+    m = len(data[-1])
+    for i in range(m):
+        for j in range(i+1):
+            left = 0
+            right = 0
+            if (j>0):
+                left = dict[(i-1,j-1)]
+            if (j<i):
+                right = dict[(i-1,j)]
+            parent = max(left, right)
+            dict[(i,j)] = parent + data[i][j]
+    maxvalue = max(dict.values())
+    print(maxvalue)
 
 if __name__ == "__main__":
+    #problem_13()
     #problem_15()
     #problem_16()
     #problem_17()
-    problem_19()
+    #problem_18()
+    #problem_19()
+    #problem_22()
+     problem_23()
+    #problem_67()
