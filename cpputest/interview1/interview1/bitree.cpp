e1f9685d033d48b22e360549f2df10d8a5bfba4e
Junch <wolf76@wolf76tekiiMac.local>
2013-08-05 11:29:23 +0800

    BiTree update

--- cpputest/interview1/interview1/bitree.cpp
+++ cpputest/interview1/interview1/bitree.cpp
@@ -29,3 +29,60 @@ BiTree CreateBiTree(BiTree& T, char* sz, int& n)
     return T;
 }
 
+void DestoryBiTree(BiTree T) // PostOrder
+{
+    if (T)
+    {
+        DestoryBiTree(T->lchild);
+        DestoryBiTree(T->rchild);
+        delete T;
+    }
+}
+
+static char buffer[1024];
+static int charnum = 0;
+void Print(BiTNode * elem)
+{
+    assert(elem != NULL);
+    
+    buffer[charnum++] = elem->data;
+    buffer[charnum] = '\0';
+}
+
+void PreOrderTraverse(BiTree T, void (Visit)(BiTNode *))
+{
+    if (T){
+        Visit(T);
+        PreOrderTraverse(T->lchild, Visit);
+        PreOrderTraverse(T->rchild, Visit);
+    }
+}
+
+TEST(BiTree, CreateBiTree)
+{
+    BiTree T = NULL;
+    char p[] = "ab  cd   ";
+    int n = 0;
+    CreateBiTree((T), p, n);
+    
+    charnum = 0;
+    PreOrderTraverse(T, Print);
+    DestoryBiTree(T);
+    
+    STRCMP_EQUAL("abcd", buffer);
+}
+
+TEST(BiTree, CreateBiTree2)
+{
+    BiTree T = NULL;
+    char p[] = "ab  c d  ";
+    int n = 0;
+    CreateBiTree((T), p, n);
+    
+    charnum = 0;
+    PreOrderTraverse(T, Print);
+    DestoryBiTree(T);
+    
+    STRCMP_EQUAL("abcd", buffer);
+}
+
