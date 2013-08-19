ead495c4fd79d4bba39bb0bdf27d288801d29d5f
wolf76 <wolf76@wolf76s-Mac.local>
2013-08-19 18:00:59 +0800

    HDOJ1028: Add the code template

---
+++ cpputest/interview1/interview1/hdoj1028.cpp
@@ -0,0 +1,27 @@
+//
+//  hdoj1028.cpp
+//  interview1
+//  http://acm.hdu.edu.cn/showproblem.php?pid=1028
+//  Created by wolf76 on 8/19/13.
+//  Copyright (c) 2013 com.wolf76. All rights reserved.
+//
+
+#include <CppUTest/TestHarness.h>
+#include <iostream>
+using namespace std;
+
+namespace HDOJ1028 {
+
+    
+    
+    
+    
+    TEST_GROUP(HDOJ1028){
+        
+    };
+    
+    TEST(HDOJ1028, case1){
+        
+    }
+    
+}
