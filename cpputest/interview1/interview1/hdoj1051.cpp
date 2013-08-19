a769ff0da305ba821a4ed08a303a87f5f19f2277
Junch <wolf76@wolf76tekiiMac.local>
2013-08-19 23:21:22 +0800

    HDOJ1951: initial

---
+++ cpputest/interview1/interview1/hdoj1051.cpp
@@ -0,0 +1,25 @@
+//
+//  hdoj1051.cpp
+//  interview1
+//  http://acm.hdu.edu.cn/showproblem.php?pid=1051
+//  Created by wolf76 on 13-8-19.
+//  Copyright (c) 2013年 com.wolf76. All rights reserved.
+//
+
+#include <CppUTest/TestHarness.h>
+#include <iostream>
+using namespace std;
+
+namespace HDOJ1051 {
+
+    
+    TEST_GROUP(HDOJ1051){
+        
+    };
+    
+    TEST(HDOJ1051, case1){
+
+    }
+
+}
+
