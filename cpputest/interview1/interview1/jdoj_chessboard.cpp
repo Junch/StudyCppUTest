f6c5e5340a795406f118bd82f59e84549958f4b6
Junch <wolf76@wolf76tekiiMac.local>
2013-08-21 23:15:17 +0800

    JDOJ_CHESSBOARD: Init

---
+++ cpputest/interview1/interview1/jdoj_chessboard.cpp
@@ -0,0 +1,25 @@
+//
+//  jdoj_chessboard.cpp
+//  interview1 棋盘寻宝
+//  http://ac.jobdu.com/problem.php?cid=1048&pid=0
+//  Created by wolf76 on 13-8-21.
+//  Copyright (c) 2013年 com.wolf76. All rights reserved.
+//
+
+#include <CppUTest/TestHarness.h>
+#include <iostream>
+using namespace std;
+
+namespace JDOJ_CHESSBOARD {
+    
+
+    
+    TEST_GROUP(JDOJ_CHESSBOARD){
+        
+    };
+    
+    TEST(JDOJ_CHESSBOARD, case1){
+
+    }
+
+}//namespace
