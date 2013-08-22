ca8ba8b13cc5617c86c1591fe204474959d9e426
Junch <wolf76@wolf76tekiiMac.local>
2013-08-22 22:59:11 +0800

    HDOJ1058: INIT

---
+++ cpputest/interview1/interview1/hdoj1058.cpp
@@ -0,0 +1,25 @@
+//
+//  hdoj1058.cpp
+//  interview1
+//  http://acm.hdu.edu.cn/showproblem.php?pid=1058
+//  Created by wolf76 on 13-8-22.
+//  Copyright (c) 2013年 com.wolf76. All rights reserved.
+//
+
+#include <CppUTest/TestHarness.h>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+namespace JDOJ1058 {
+
+    
+    TEST_GROUP(JDOJ1058){
+        
+    };
+    
+    TEST(JDOJ1058, case1){
+
+    }
+    
+}//namespace
