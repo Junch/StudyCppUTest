368b804c98c1d242ace5898091ec65fea221a6c0
wolf76 <wolf76@wolf76s-Mac.local>
2013-08-28 18:08:54 +0800

    HDOJ2553: TLE

--- cpputest/interview1/interview1/hdoj2553.cpp
+++ cpputest/interview1/interview1/hdoj2553.cpp
@@ -1,28 +1,59 @@
 //
 //  hdoj2553.cpp
 //  interview1
-//
+//  http://acm.hdu.edu.cn/showproblem.php?pid=2553
 //  Created by wolf76 on 8/28/13.
 //  Copyright (c) 2013 com.wolf76. All rights reserved.
 //
 
-//
-//  hdoj1059.cpp
-//  interview1
-//  http://acm.hdu.edu.cn/showproblem.php?pid=2553
-//  Created by wolf76 on 13-8-24.
-//  Copyright (c) 2013年 com.wolf76. All rights reserved.
-//
-
 #include <CppUTest/TestHarness.h>
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 namespace HDOJ2553 {
     
+    int N=8;
+    int total = 0;
+    int a[10]; // The row queen is placed at a[row] column
+    
+    bool inline placable(int row, int col){
+        assert(row < N && col < N);
+        
+        for (int i=0; i<row; ++i) {
+            if (a[i] == col)
+                return false;
+            
+            if( row - i == abs(col - a[i]))
+                return false;
+        }
+        
+        return true;
+    }
+    
+    void dfs(int n){
+        if (n == N){
+            ++total;
+            return;
+        }
+        
+        for (int i=0; i<N; ++i) {
+            if (placable(n, i)) {
+                a[n] = i;
+                dfs(n+1);
+            }
+        }        
+    }
     
     int main()
     {
+        int n;
+        while (cin >> n && n) {
+            N = n;
+            total = 0;
+            dfs(0);
+            cout << total << endl;
+        }
         
         return 0;
     }
@@ -42,7 +73,24 @@ namespace HDOJ2553 {
     }
     
     TEST(HDOJ2553, case1){
-        FAIL("HDOJ2553 unsolved.");
+        N = 5;
+        total = 0;
+        dfs(0);
+        LONGS_EQUAL(10, total);
+    }
+    
+    TEST(HDOJ2553, case2){
+        N = 8;
+        total = 0;
+        dfs(0);
+        LONGS_EQUAL(92, total);
+    }
+    
+    TEST(HDOJ2553, case3){
+        N = 1;
+        total = 0;
+        dfs(0);
+        LONGS_EQUAL(1, total);
     }
     
 }//namespace
