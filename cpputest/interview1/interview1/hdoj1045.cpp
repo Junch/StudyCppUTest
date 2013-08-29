2036f5f8101210d4e2b7893e6c1e983c6b02306f
Junch <wolf76@wolf76tekiiMac.local>
2013-08-30 00:06:52 +0800

    HDOJ1045: AC

--- cpputest/interview1/interview1/hdoj1045.cpp
+++ cpputest/interview1/interview1/hdoj1045.cpp
@@ -8,14 +8,83 @@
 
 #include <CppUTest/TestHarness.h>
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 namespace HDOJ1045 {
+    char board[4][4];
+    int  citySize;
+    int  maximum=0;
+    int  count=0;
     
+    bool place(int index){
+        assert(index < citySize*citySize);
+        int row = index/citySize;
+        int col = index % citySize;
+        
+        if (board[row][col]=='X')
+            return false;
+        
+        return true;
+    }
+    
+    void dfs(int index){
+        assert(index <= citySize*citySize);
+        if (index == citySize*citySize){
+            if (count>maximum)
+                maximum = count;
+            
+            return;
+        }
+        
+        // place a blockhouse or not to place the blockhouse
+        dfs(index+1);
+        if (place(index)) {
+            int row = index/citySize;
+            int col = index % citySize;
+            
+            board[row][col]='X';
+            ++count;
+            
+            int i=row+1;
+            for (;i<citySize;++i)
+                if (board[i][col]=='.')
+                    board[i][col]='X';
+                else if(board[i][col]=='X')
+                    break;
+            
+            int j=col+1;
+            for (;j<citySize;++j)
+                if (board[row][j]=='.')
+                    board[row][j]='X';
+                else if(board[row][j]=='X')
+                    break;
+            
+            dfs(index+1);
+            
+            board[row][col]='.';
+            for (int k=row+1; k<i; k++)
+                board[k][col]='.';
+            for (int k=col+1; k<j; k++)
+                board[row][k]='.';
+            --count;
+        }
+    }
     
     
     int main()
     {
+        int n;        
+        while (cin>>n && n) {
+            for (int i=0;i<n;++i)
+                for (int j=0; j<n;++j)
+                    cin >> board[i][j];
+
+            citySize = n;
+            maximum = 0;
+            dfs(0);
+            printf("%d\n",maximum);
+        }
         
         return 0;
     }
@@ -35,7 +104,23 @@ namespace HDOJ1045 {
     }
     
     TEST(HDOJ1045, case1){
-        FAIL("HDOJ1045 unsolved.");
+        memset(board, '.', sizeof(board));
+        board[1][1]=board[1][2]='X';
+        board[2][1]=board[2][2]='X';
+        citySize = 3;
+        maximum = 0;
+        dfs(0);
+        LONGS_EQUAL(2, maximum);
+    }
+    
+    TEST(HDOJ1045, case2){       
+        memset(board, '.', sizeof(board));
+        board[0][1]='X';
+        board[2][0]=board[2][1]='X';
+        citySize = 4;
+        maximum = 0;
+        dfs(0);
+        LONGS_EQUAL(5, maximum);
     }
     
 }//namespace
