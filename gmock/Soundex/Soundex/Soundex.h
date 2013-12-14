28735ca0c68aa5a6f1ac8e4a7ae67fa302acde9d
Jun Chen <jun@JundeMac.local>
2013-12-14 18:55:27 +0800

    Soundex: 7th: refactor to a more declarative solution

--- gmock/Soundex/Soundex/Soundex.h
+++ gmock/Soundex/Soundex/Soundex.h
@@ -15,14 +15,20 @@ class Soundex
 {
 public:
     std::string encode(const std::string& word) const {
-        auto encoded = word.substr(0, 1);
-        if (word.length() > 1)
-            encoded += "1";
-        
-        return zeroPad(encoded);
+        return zeroPad(head(word)+ encodedDigits(word));
     }
     
 private:
+    std::string head(const std::string& word) const {
+        return word.substr(0, 1);
+    }
+    
+    std::string encodedDigits(const std::string& word) const {
+        if (word.length() > 1)
+            return "1";
+        return "";
+    }
+    
     std::string zeroPad(const std::string &word) const{
         auto zerosNeeded = 4 - word.length();
         return word + std::string(zerosNeeded, '0');
