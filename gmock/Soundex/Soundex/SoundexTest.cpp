1aa3d629cf09d7b522b1241f03af675ee8213043
Jun Chen <jun@JundeMac.local>
2013-12-14 16:47:35 +0800

    Soundex: 4th: refactoring: add a new method and a test fixture

--- gmock/Soundex/Soundex/SoundexTest.cpp
+++ gmock/Soundex/Soundex/SoundexTest.cpp
@@ -12,22 +12,31 @@ using ::testing::Eq;
 class Soundex
 {
 public:
-    std::string encode(const std::string& word)const {
+    std::string encode(const std::string& word) const {
+        return zeroPad(word);
+    }
+    
+private:
+    std::string zeroPad(const std::string &word) const{
         return word + "000";
     }
 };
 
 
-TEST(SoundexEncoding, RetainSoleLetterOfOneLetterWord)
+class SoundexEncoding: public testing::Test
 {
+public:
     Soundex soundex;
+};
+
+TEST_F(SoundexEncoding, RetainSoleLetterOfOneLetterWord)
+{
     auto encoded=soundex.encode("A");
     ASSERT_THAT(encoded, Eq("A000"));
 }
 
-TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
 {
-    Soundex soundex;
     auto encoded=soundex.encode("I");
     ASSERT_THAT(encoded, Eq("I000"));
 }
