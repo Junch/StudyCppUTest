55eabd0bf898764f6a6758d97fef3dcce9ea8f15
Jun Chen <jun@JundeMac.local>
2013-12-14 14:18:02 +0800

    Soundex: 3rd: #4 fills in zeros until there are three numbers

--- gmock/Soundex/Soundex/SoundexTest.cpp
+++ gmock/Soundex/Soundex/SoundexTest.cpp
@@ -13,7 +13,7 @@ class Soundex
 {
 public:
     std::string encode(const std::string& word)const {
-        return word;
+        return word + "000";
     }
 };
 
@@ -22,8 +22,14 @@ TEST(SoundexEncoding, RetainSoleLetterOfOneLetterWord)
 {
     Soundex soundex;
     auto encoded=soundex.encode("A");
-    ASSERT_THAT(encoded, Eq("A"));
+    ASSERT_THAT(encoded, Eq("A000"));
 }
 
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
+{
+    Soundex soundex;
+    auto encoded=soundex.encode("I");
+    ASSERT_THAT(encoded, Eq("I000"));
+}
 
 
