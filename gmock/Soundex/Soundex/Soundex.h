aab7861facac003609c49d5e54df8fa4d5fd5f97
Jun Chen <jun@JundeMac.local>
2013-12-14 19:00:21 +0800

    Soundex: 8th: refactor to eliminate the hard code

--- gmock/Soundex/Soundex/Soundex.h
+++ gmock/Soundex/Soundex/Soundex.h
@@ -19,18 +19,24 @@ public:
     }
     
 private:
+    static const size_t MaxCodeLength{4};
+    
     std::string head(const std::string& word) const {
         return word.substr(0, 1);
     }
     
     std::string encodedDigits(const std::string& word) const {
         if (word.length() > 1)
-            return "1";
+            return encodedDigit();
         return "";
     }
     
+    std::string encodedDigit() const {
+        return "1";
+    }
+    
     std::string zeroPad(const std::string &word) const{
-        auto zerosNeeded = 4 - word.length();
+        auto zerosNeeded = MaxCodeLength - word.length();
         return word + std::string(zerosNeeded, '0');
     }
 };
