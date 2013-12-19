45778ea5c569cdf786ce81c14aca693d4570658f
Jun Chen <jun@JundeMac.local>
2013-12-19 10:52:43 +0800

    testDouble: Update the test which will cause test fail

--- gmock/testDouble/1/PlaceDescriptionServiceTest.cpp
+++ gmock/testDouble/1/PlaceDescriptionServiceTest.cpp
@@ -16,18 +16,36 @@ const string APlaceDescriptionService::ValidLatitude("38.005");
 const string APlaceDescriptionService::ValidLongitude("-104.44");
 
 class HttpStub: public Http {
+public:
+   string returnResponse;
+   string expectedURL;
+
    void initialize() override {}
    std::string get(const std::string& url) const override {
-      return R"({ "address": {
-         "road":"Drury Ln",
-         "city":"Fountain",
-         "state":"CO",
-         "country":"US" }})";
+      verify(url);
+      return returnResponse;
+   }
+
+   void verify(const string&url) const{
+      ASSERT_THAT(url, Eq(expectedURL));
    }
 };
 
 TEST_F(APlaceDescriptionService, ReturnsDescriptionForValidLocation) {
    HttpStub httpStub;
+   httpStub.returnResponse = R"({"address": {
+                                    "road":"Drury Ln",
+                                    "city":"Fountain",
+                                    "state":"CO",
+                                    "country":"US" }})";
+
+   string urlStart{
+      "http://open.mapquestapi.com/nominatim/v1/reverse?format=json&"};
+
+   httpStub.expectedURL = urlStart + 
+      "lat=" + APlaceDescriptionService::ValidLatitude + "&" +
+      "lon=" + APlaceDescriptionService::ValidLongitude;
+
    PlaceDescriptionService service{&httpStub};
 
    auto description = service.summaryDescription(ValidLatitude, ValidLongitude);
