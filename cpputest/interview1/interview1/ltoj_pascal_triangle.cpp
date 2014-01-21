708f73fe600184aebe2b12b98e9e84d14443d129
Jun Chen <Junc76@gmail.com>
2014-01-21 11:11:49 +0800

    LTOJ: Pascal Triangle: AC

--- cpputest/interview1/interview1/ltoj_pascal_triangle.cpp
+++ cpputest/interview1/interview1/ltoj_pascal_triangle.cpp
@@ -16,7 +16,7 @@ using namespace std;
 namespace LTOJ_PASCAL_TRIANGLE {
     class Solution {
     public:
-        vector<vector<int> > generate(int numRows){
+        vector<vector<int> > generateSlow(int numRows){
             vector<vector<int>> vv;
            
             for (int i=0; i<numRows; ++i) {
@@ -31,6 +31,22 @@ namespace LTOJ_PASCAL_TRIANGLE {
                 }
                 
                 vv[i].push_back(1);
+            }
+            
+            return vv;
+        }
+        
+        vector<vector<int> > generate(int numRows){
+            vector<vector<int>> vv(numRows);
+            
+            for (int i=0; i<numRows; ++i) {
+                vv[i].resize(i+1);
+                vv[i][0] = 1;
+
+                for (int j=1; j<i; ++j)
+                    vv[i][j] = vv[i-1][j-1] + vv[i-1][j];
+                
+                vv[i][i] = 1;
             }
             
             return vv;
