62a0fffc6b6be5bee2e32e4d1bfdd47c2ef4d4a1
Jun Chen <Junc76@gmail.com>
2014-01-23 16:06:38 +0800

    C++11: Add an example to enable_shared_from_this

--- gmock/Cpp11/Cpp11/shared_ptr.cpp
+++ gmock/Cpp11/Cpp11/shared_ptr.cpp
@@ -207,7 +207,7 @@ TEST(shared_ptr, preventDeleteRawPointer)
     shared_ptr<X> x= X::create();
 }
 
-TEST(shared_ptr, test_weak_ptr)
+TEST(shared_ptr, weak_ptr)
 {
     // std::weak_ptr 用来表达临时所有权的概念：当某个对象只有存在时才需要被访问，
     // 而且随时可能被他人删除时，可以使用 std::weak_ptr 来跟踪该对象。需要获得
@@ -235,3 +235,47 @@ TEST(shared_ptr, test_weak_ptr)
     func();
 }
 
+TEST(shared_ptr, enable_shared_from_this)
+{
+    // 使用情景：当类对象被 shared_ptr 管理时，需要在类自己定义的函数里把当前类对象作为参数
+    // 传给其他函数时，这时需要传递一个 shared_ptr ，否则就不能保持 shared_ptr 管理这个类
+    // 对象的语义（因为有一个 raw pointer 指向这个类对象，而 shared_ptr 对类对象的这个引
+    // 用没有计数，很有可能 shared_ptr 已经把类对象资源释放了，而那个调用函数还在使用类对象
+    
+    // The key point is that the "obvious" technique of just returning shared_ptr<Y>(this)
+    // is broken, because this winds up creating multiple distinct shared_ptr objects with
+    // separate reference counts. For this reason you must never create more than one
+    // shared_ptr from the same raw pointer.
+    
+    struct Good: enable_shared_from_this<Good>
+    {
+        shared_ptr<Good> getptr() {
+            return shared_from_this();
+        }
+    };
+    
+    struct Bad
+    {
+        shared_ptr<Bad> getptr() {
+            return shared_ptr<Bad>(this);
+        }
+        ~Bad() { std::cout << "Bad::~Bad() called\n"; }
+    };
+    
+    // Good: the two shared_ptr's share the same object
+    std::shared_ptr<Good> p1(new Good);
+    std::shared_ptr<Good> p2 = p1->getptr();
+    std::cout << "p2.use_count() = " << p2.use_count() << '\n';
+    
+    auto func = [](Good * g){
+        shared_ptr<Good> p3 = g->getptr();
+        std::cout << "p3.use_count() = " << p3.use_count() << '\n';
+    };
+    
+    func(p1.get());
+
+    // Bad, each shared_ptr thinks it's the only owner of the object
+    // std::shared_ptr<Bad> bp1(new Bad);
+    // std::shared_ptr<Bad> bp2 = bp1->getptr();
+    // std::cout << "bp2.use_count() = " << bp2.use_count() << '\n';
+}
