d9917f56c90baa35826a48eb395a6cb4b14e5b14
Jun Chen <Junc76@gmail.com>
2014-01-26 18:27:01 +0800

    LTOJ: Word Break: AC (DP)

--- cpputest/interview1/interview1/ltoj_word_break.cpp
+++ cpputest/interview1/interview1/ltoj_word_break.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <unordered_set>
+#include <vector>
 #include <CppUTest/TestHarness.h>
 using namespace std;
 
@@ -23,13 +24,15 @@ namespace LTOJ_WORD_BREAK {
                 return false;
             
             for (size_t i=1, len=s.length(); i<=len; ++i) {
+                // string head = string(s.begin(), s.begin()+i);
                 string head = s.substr(0, i);
                 if (dict.find(head) == dict.end())
                     continue;
                 
                 dict.erase(head);
                 
-                string rear = s.substr(i, len);;
+                // string rear = string(s.begin()+i, s.end());
+                string rear = s.substr(i, len-i);
                 if (wordBreak(rear, dict))
                     return true;
                 
@@ -37,6 +40,31 @@ namespace LTOJ_WORD_BREAK {
             }
             
             return false;
+        }
+        
+        bool wordBreak2(string s, unordered_set<string> &dict) {
+            size_t len = s.length();
+            if (len == 0)
+                return false;
+            
+            vector<bool> v(len);
+            
+            for (int i=0; i<len; ++i) {
+                v[i] = false;
+                
+                for (int j=i; j>= 0; --j) {
+                    if (j != 0 && v[j-1] == false)
+                        continue;
+                    
+                    string tmp = s.substr(j, i-j+1);
+                    if (dict.find(tmp) != dict.end()) {
+                        v[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return v[len-1];
         }
     };
     
@@ -67,5 +95,34 @@ namespace LTOJ_WORD_BREAK {
     TEST(LTOJ_WORD_BREAK, MoreWords){
         unordered_set<string> dict{"a"};
         CHECK_FALSE(sln.wordBreak("aa", dict));
+    }
+    
+    TEST_GROUP(LTOJ_WORD_BREAK2){
+        Solution sln;
+    };
+    
+    TEST(LTOJ_WORD_BREAK2, OneWord){
+        unordered_set<string> dict{"ab"};
+        CHECK_TRUE(sln.wordBreak2("ab", dict));
+    }
+    
+    TEST(LTOJ_WORD_BREAK2, TwoWords){
+        unordered_set<string> dict{"a", "b"};
+        CHECK_TRUE(sln.wordBreak2("ab", dict));
+    }
+    
+    TEST(LTOJ_WORD_BREAK2, ThreeWordsFalse){
+        unordered_set<string> dict{"a", "b", "c", "d"};
+        CHECK_FALSE(sln.wordBreak2("abe", dict));
+    }
+    
+    TEST(LTOJ_WORD_BREAK2, ThreeWordsTrue){
+        unordered_set<string> dict{"a", "bd"};
+        CHECK_TRUE(sln.wordBreak2("abd", dict));
+    }
+    
+    IGNORE_TEST(LTOJ_WORD_BREAK2, MoreWords){
+        unordered_set<string> dict{"a"};
+        CHECK_FALSE(sln.wordBreak2("aa", dict));
     }
 }
