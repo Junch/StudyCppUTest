b6d226e7fe5e50a9198f7621f3007efbe8e819ef
Jun Chen <jun@JundeMac.local>
2014-01-27 22:34:35 +0800

    LTOJ: Jump Game II: AC

--- cpputest/interview1/interview1/ltoj_jump_game_ii.cpp
+++ cpputest/interview1/interview1/ltoj_jump_game_ii.cpp
@@ -1,6 +1,7 @@
 //
 //  ltoj_jump_game_ii.cpp
 //  interview1
+//  http://oj.leetcode.com/problems/jump-game-ii/
 //
 //  Created by Jun on 14-1-27.
 //  Copyright (c) 2014年 com.wolf76. All rights reserved.
@@ -35,22 +36,117 @@ namespace LTOJ_JUMP_GAME_II {
         }
     };
     
-    TEST_GROUP(LTOJ_JUMP_GAME_II){
-        Solution sln;
+    class Solution2 {
+    public:
+        int jump(int A[], int n) {
+            
+            if (n == 1)
+                return 0;
+            
+            int max = 0;
+            int level = -1;
+            int levelIndex = -1;
+            
+            for (int i=0; i<n-1 ; ++i) {
+                if (i > levelIndex){
+                    ++level;
+                    levelIndex = max;
+                }
+                
+                int pos = A[i] + i;
+                if (pos > max)
+                    max = pos;
+                
+                if (i > max)
+                    return 0;
+                
+                if (max >= n-1) {
+                    return level+1;
+                }
+            }
+            
+            return level;
+        }
     };
     
-    TEST(LTOJ_JUMP_GAME_II, 2Step){
+    class Solution3 {
+    public:
+        int jump(int A[], int n) {
+
+            int max = 0;
+            int level = 0;
+            int levelIndex = -1;
+            
+            for (int i=0; i<n-1 && max<n-1; ++i) {
+                if (i > levelIndex){
+                    ++level;
+                    levelIndex = max;
+                }
+                
+                max = std::max(max, A[i] + i);
+            }
+            
+            return level;
+        }
+    };
+    
+    
+    /*
+     * We use "last" to keep track of the maximum distance that has been reached
+     * by using the minimum steps "ret", whereas "curr" is the maximum distance
+     * that can be reached by using "ret+1" steps. Thus,
+     * curr = max(curr, i+A[i]) where 0 <= i <= last.
+     */
+    class Solution4 {
+    public:
+        int jump(int A[], int n) {
+            int ret = 0;
+            int last = 0;
+            int curr = 0;
+            for (int i = 0; i < n; ++i) {
+                if (i > last) {
+                    last = curr;
+                    ++ret;
+                }
+                curr = max(curr, i+A[i]);
+            }
+            
+            return ret;
+        }
+    };
+    
+    TEST_GROUP(LTOJ_JUMP_GAME_II){
+        Solution4 sln;
+    };
+    
+    TEST(LTOJ_JUMP_GAME_II, 23114){
         int A[]={2,3,1,1,4};
         LONGS_EQUAL(2, sln.jump(A, 5));
     }
     
-    TEST(LTOJ_JUMP_GAME_II, 3Step){
+    TEST(LTOJ_JUMP_GAME_II, 20114){
         int A[]={2,0,1,1,4};
         LONGS_EQUAL(3, sln.jump(A, 5));
     }
-    
-    TEST(LTOJ_JUMP_GAME_II, 0Step){
+
+    TEST(LTOJ_JUMP_GAME_II, 2){
         int A[]={2};
         LONGS_EQUAL(0, sln.jump(A, 1));
     }
+    
+    TEST(LTOJ_JUMP_GAME_II, 11){
+        int A[]={1,1};
+        LONGS_EQUAL(1, sln.jump(A, 2));
+    }
+    
+    TEST(LTOJ_JUMP_GAME_II, 111){
+        int A[]={1,1,1};
+        LONGS_EQUAL(2, sln.jump(A, 3));
+    }
+    
+    TEST(LTOJ_JUMP_GAME_II, 1111){
+        int A[]={1,1,1,1};
+        LONGS_EQUAL(3, sln.jump(A, 4));
+    }
+
 }
