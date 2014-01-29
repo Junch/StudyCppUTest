5c0e4a0bc3f93aecadcff6f87ea30cd2d9df43d1
Jun Chen <Junc76@gmail.com>
2014-01-29 17:02:01 +0800

    C++11: add rvalue reference (blank)

---
+++ gmock/Cpp11/Cpp11/rvalue_reference.cpp
@@ -0,0 +1,19 @@
+//
+//  rvalue_reference.cpp
+//  Cpp11
+//
+//  Created by Jun on 14-1-29.
+//  Copyright (c) 2014年 Jun. All rights reserved.
+//
+
+#include <gtest/gtest.h>
+#include <memory>
+using namespace testing;
+using namespace std;
+
+namespace rvalue_reference {
+    
+    
+    
+    
+}
