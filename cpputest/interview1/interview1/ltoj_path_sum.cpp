2dd178f2c583daed400638a1a5954805cb7800ec
Jun Chen <jun@JundeMac.local>
2014-02-04 18:25:01 +0800

    LTOJ: Path Sum II

--- cpputest/interview1/interview1/ltoj_path_sum.cpp
+++ cpputest/interview1/interview1/ltoj_path_sum.cpp
@@ -1,6 +1,7 @@
 //
 //  ltoj_path_sum.cpp
 //  interview1
+//  http://oj.leetcode.com/problems/path-sum-ii/
 //
 //  Created by Jun on 14-1-31.
 //  Copyright (c) 2014年 com.wolf76. All rights reserved.
@@ -55,6 +56,53 @@ namespace LTOJ_PATH_SUM {
         }
     };
     
+    class Solution2 {
+    public:
+        vector<vector<int> > pathSum(TreeNode *root, int sum) {
+            vector<vector<int>> vv;
+            
+            if (root == nullptr)
+                return vv;
+            
+            queue<tuple<TreeNode*, int, vector<int>>> Q;
+            
+            vector<int> v{root->val};
+            Q.push(make_tuple(root, root->val, v));
+            
+            while (!Q.empty()) {
+                auto& top = Q.front();
+                
+                TreeNode* node = get<0>(top);
+                int val = get<1>(top);
+                const auto& v = get<2>(top);
+                
+                TreeNode* l = node->left;
+                TreeNode* r = node->right;
+                if (!l && !r && val==sum)
+                    vv.push_back(v);
+                
+                if (l) {
+                    int newVal = val + l->val;
+                    auto lv = v;
+                    lv.push_back(l->val);
+                    Q.push(make_tuple(l, newVal, lv));
+                }
+
+                if (r) {
+                    int newVal = val + r->val;
+                    auto rv = v;
+                    rv.push_back(r->val);
+                    Q.push(make_tuple(r, newVal, rv));
+                }
+                
+                Q.pop();
+            }
+            
+            return vv;
+        }
+    };
+
+    
     TEST_GROUP(LTOJ_PATH_SUM){
         Solution sln;
     };
@@ -67,6 +115,33 @@ namespace LTOJ_PATH_SUM {
     }
     
     TEST(LTOJ_PATH_SUM, NoElems){
-        CHECK_TRUE(sln.hasPathSum(nullptr, 1));
+        CHECK_FALSE(sln.hasPathSum(nullptr, 1));
+    }
+    
+    
+    
+    TEST_GROUP(LTOJ_PATH_SUM_II){
+        Solution2 sln;
+    };
+    
+    TEST(LTOJ_PATH_SUM_II, NoElems){
+        LONGS_EQUAL(0, sln.pathSum(nullptr, 1).size());
+    }
+    
+    TEST(LTOJ_PATH_SUM_II, 2elems){
+        TreeNode t0(1);
+        TreeNode t1(2);
+        t0.right = &t1;
+        auto && v = sln.pathSum(&t0, 3);
+        LONGS_EQUAL(1, v.size());
+        LONGS_EQUAL(1, v[0][0]);
+        LONGS_EQUAL(2, v[0][1]);
+    }
+    
+    TEST(LTOJ_PATH_SUM_II, 1elems){
+        TreeNode t0(1);
+        auto && v = sln.pathSum(&t0, 1);
+        LONGS_EQUAL(1, v.size());
+        LONGS_EQUAL(1, v[0][0]);
     }
 }
