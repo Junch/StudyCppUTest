ba6e96e35f5c594cf4a8bae9d36346d91f8d2374
Jun Chen <jun@JundeMac.local>
2014-02-23 21:58:36 +0800

    LTOJ: Word Search: AC

--- cpputest/interview1/interview1/ltoj_word_search.cpp
+++ cpputest/interview1/interview1/ltoj_word_search.cpp
@@ -76,14 +76,80 @@ namespace LTOJ_WORD_SEARCH {
         }
     };
     
+    class Solution2 {
+    public:
+        
+        bool exist(vector<vector<char> > &board, string word) {
+            size_t width = board[0].size();
+            size_t height = board.size();
+            
+            for (int j=0; j<height; ++j) {
+                for (int i=0; i<width; ++i) {
+                    char c = board[j][i];
+                    if (c==word[0]) {
+                        board[j][i] = '#';
+                        bool ret = dfs(board, word, 0, i, j);
+                        if (ret)
+                            return true;
+                        board[j][i] = c;
+                    }
+                }
+            }
+            
+            return false;
+        }
+        
+        
+        bool dfs(vector<vector<char> > &board,  const string& word, int index, int x, int y)
+        {
+            static int dirs[][2]={{-1,0}, {1,0}, {0, -1}, {0, 1}};
+            size_t width = board[0].size();
+            size_t height = board.size();
+            
+            if (index+1 == word.length())
+                return true;
+            
+            for (int i=0; i<4; ++i) {
+                int nx = x + dirs[i][0];
+                int ny = y + dirs[i][1];
+                
+                if (nx < 0 || nx >= width ||
+                    ny < 0 || ny >= height)
+                    continue;
+                
+                if (board[ny][nx] == word[index + 1]) {
+                    board[ny][nx] = '#';
+                    if (dfs(board, word, index+1, nx, ny))
+                        return true;
+                    board[ny][nx] = word[index + 1];
+                }
+            }
+            
+            return false;
+        }
+    };
+    
     TEST_GROUP(LTOJ_WORD_SEARCH){
-        Solution sln;
+        Solution2 sln;
     };
     
     TEST(LTOJ_WORD_SEARCH, case1){
         vector<vector<char>> vv;
         vv.push_back(vector<char>{'A','B'});
         vv.push_back(vector<char>{'C','C'});
-        CHECK_TRUE(sln.exist(vv, "CCA"));
+        CHECK_TRUE(sln.exist(vv, "CBA"));
+    }
+    
+    TEST(LTOJ_WORD_SEARCH, case2){
+        vector<vector<char>> vv;
+        vv.push_back(vector<char>{'A','B'});
+        CHECK_TRUE(sln.exist(vv, "AB"));
+    }
+    
+    TEST(LTOJ_WORD_SEARCH, case3){
+        vector<vector<char>> vv;
+        vv.push_back(vector<char>{'A'});
+        vv.push_back(vector<char>{'B'});
+        CHECK_TRUE(sln.exist(vv, "AB"));
     }
 }
