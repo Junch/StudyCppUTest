7b0857528046f39bbd99d1b8dd37ba29068526a5
Jun Chen <jun@JundeMac.local>
2014-02-23 23:00:50 +0800

    LTOJ: Word Search: AC: Use stack instead of recursion

--- cpputest/interview1/interview1/ltoj_word_search.cpp
+++ cpputest/interview1/interview1/ltoj_word_search.cpp
@@ -129,15 +129,86 @@ namespace LTOJ_WORD_SEARCH {
         }
     };
     
+    class Solution3 {
+    public:
+        
+        bool exist(vector<vector<char> > &board, string word) {
+            size_t width = board[0].size();
+            size_t height = board.size();
+            
+            for (int j=0; j<height; ++j) {
+                for (int i=0; i<width; ++i) {
+                    char c = board[j][i];
+                    if (c==word[0]) {
+                        bool ret = process(board, word, i, j);
+                        if (ret)
+                            return true;
+                    }
+                }
+            }
+            
+            return false;
+        }
+        
+        
+        bool process(vector<vector<char> > &board,  const string& word, int x, int y)
+        {
+            static int dirs[][2]={{-1,0}, {1,0}, {0, -1}, {0, 1}};
+            size_t width = board[0].size();
+            size_t height = board.size();
+
+            struct Node{
+                int x;
+                int y;
+                int dir;
+            };
+            
+            stack<Node> s;
+            board[y][x] = '#';
+            s.push(Node{x, y, 0});
+            
+            while (!s.empty()) {
+                Node& t = s.top();
+                
+                size_t index = s.size();
+                if (index == word.length())
+                    return true;
+                
+                if (t.dir == 4) {
+                    board[t.y][t.x] = word[index-1];
+                    s.pop();
+                    continue;
+                }
+                
+                int nx = t.x + dirs[t.dir][0];
+                int ny = t.y + dirs[t.dir][1];
+                ++t.dir;
+                
+                if (nx < 0 || nx >= width ||
+                    ny < 0 || ny >= height) {
+                    continue;
+                }
+                
+                if (board[ny][nx] == word[index]) {
+                    board[ny][nx] = '#';
+                    s.push(Node{nx, ny, 0});
+                }
+            }
+            
+            return false;
+        }
+    };
+
+    
     TEST_GROUP(LTOJ_WORD_SEARCH){
-        Solution2 sln;
+        Solution3 sln;
     };
     
     TEST(LTOJ_WORD_SEARCH, case1){
         vector<vector<char>> vv;
         vv.push_back(vector<char>{'A','B'});
-        vv.push_back(vector<char>{'C','C'});
-        CHECK_TRUE(sln.exist(vv, "CBA"));
+        vv.push_back(vector<char>{'C','D'});
+        CHECK_TRUE(sln.exist(vv, "DCBA"));
     }
     
     TEST(LTOJ_WORD_SEARCH, case2){
