bb8678d47a83cb1534e2d74b214e2f4e456e94da
Jun Chen <Jun@JundeiMac.local>
2015-02-18 22:11:59 +0800

    LeetCode: Merge k Sorted Lists: AC

--- gmock/leetcode/MergekSortedLists.cpp
+++ gmock/leetcode/MergekSortedLists.cpp
@@ -23,22 +23,22 @@ struct ListNode {
     
 class Solution {
 public:
+
     ListNode *mergeKLists(vector<ListNode *> &lists) {
-        if (lists.size() == 0) {
-            return nullptr;
-        }
-        
-        ListNode* r = lists[0];
-        for (int i=1; i<lists.size(); ++i){
-            r = mergeLists(r, lists[i]);
-        }
-        
-        return r;
+        return merge(begin(lists), end(lists));
+    }
+    
+    ListNode *merge(vector<ListNode*>::iterator b, vector<ListNode*>::iterator e){
+        if(b==e)    return nullptr;
+        if(next(b)==e)  return *b;
+    
+        auto m=b+(e-b)/2;
+        return mergeLists(merge(b, m), merge(m,e));
     }
     
     ListNode* mergeLists(ListNode* p, ListNode* q){
-        ListNode* h = new ListNode(0);
-        ListNode* r = h;
+        ListNode h(0);
+        ListNode* r = &h;
         
         while (p != nullptr && q != nullptr) {
             if (p->val < q->val){
@@ -58,10 +58,56 @@ public:
             r->next = q;
         }
         
-        r = h->next;
-        delete h;
+        return h.next;
+    }
+    
+    ListNode* mergeLists2(ListNode* p, ListNode* q){
+        // The optimization seems to be not necessary indeeded.
+        
+        ListNode h(0);
+        ListNode* r = &h;
+        
+        while (p != nullptr && q != nullptr) {
+            if (p->val < q->val){
+                r->next = p;
+                while (r->next != nullptr &&  r->next->val <= q->val) {
+                    r = r->next;
+                }
+
+                p = r->next;
+            }else{
+                r->next = q;
+                
+                while (r->next != nullptr &&  r->next->val <= p->val) {
+                    r = r->next;
+                }
+             
+                q = r->next;
+            }
+        }
+        
+        if (p != nullptr) {
+            r->next = p;
+        }else{
+            r->next = q;
+        }
+
+        return h.next;
+    }
+    
+    ListNode *mergeKLists2(vector<ListNode *> &lists) {
+        if (lists.size() == 0) {
+            return nullptr;
+        }
+        
+        ListNode* r = lists[0];
+        for (int i=1; i<lists.size(); ++i){
+            r = mergeLists(r, lists[i]);
+        }
+        
         return r;
     }
+    
 };
     
 
@@ -79,7 +125,6 @@ bool listAreEqual(ListNode* p, const std::array<int, size>& arr){
     
     return p == nullptr && i == arr.size() ;
 }
-    
 
 template <size_t size>
 ListNode* createList(const std::array<int, size>& arr){
@@ -95,21 +140,21 @@ ListNode* createList(const std::array<int, size>& arr){
     delete h;
     return p;
 }
- 
-
-TEST(MergekSortedLists, case1) {
-    ADD_FAILURE()<< "Not implemented";
+    
+TEST(MergekTwoLists, mergeLists) {
+    auto p = createList(std::array<int,3> {1, 2, 2});
+    auto q = createList(std::array<int,3> {1, 1, 2});
+    Solution* sln = new Solution();
+    auto r = sln->mergeLists(p, q);
+    ASSERT_TRUE(listAreEqual(r, std::array<int, 6>{1, 1, 1, 2, 2, 2}));
 }
-
-TEST(MergekTwoLists, mergeTwoLists) {
+    
+TEST(MergekTwoLists, mergeLists2) {
     auto p = createList(std::array<int,2> {1, 4});
     auto q = createList(std::array<int,2> {2, 3});
     Solution* sln = new Solution();
-    auto r = sln->mergeLists(p, q);
+    auto r = sln->mergeLists2(p, q);
     ASSERT_TRUE(listAreEqual(r, std::array<int, 4>{1, 2, 3, 4}));
-    
-    //std::array<int, 3> temp {3, 4, 5};
-    //ASSERT_THAT(temp, ElementsAre(3, 4, 5));
 }
     
 TEST(MergekTwoLists, listAreEqual) {
